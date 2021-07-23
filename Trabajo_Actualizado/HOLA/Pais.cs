6a992fbb728b41a90483083a52b58e812be1e015
alvaroisraelc26 <alvaroisraelc26@gmail.com>
2021-07-23 14:25:16 -0500

    Trabajo_Actualizado

---
+++ Trabajo_Actualizado/HOLA/Pais.cs
@@ -0,0 +1,15 @@
+﻿using  System.Collections.Generic;
+
+namespace MisPrimerosPasos1
+{
+    class Pais
+    {
+        public int PaisId { get; set; }
+
+        public string Nombre {get; set; }
+
+        public List<string> Provincia {get; set; }
+        
+        
+    } 
+}
---
+++ Trabajo_Actualizado/HOLA/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MisPrimerosPasos1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pais ="Ecuador";
+            Pais  = new Pais();
+
+            Pais pais1;
+            var pais2;
+
+            var pais = new Pais();
+            pais.Nombre = "Ecuador";
+            pais.Provincia = new List<string>();
+            pais.Provincia.Add("Pichincha");
+            pais.Provincia.Add("Galapagos");
+            pais.Provincia.Add("Loja");
+
+        }
+    }   
+}
